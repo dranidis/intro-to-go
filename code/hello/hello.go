d618d4e289c113cbe267ea627c2879b828d043b1
Dimitris Dranidis <dranidis@gmail.com>
2023-04-07 18:08:09 +0300

    Remove slides

--- code/hello/hello.go
+++ code/hello/hello.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello, κόσμε!\n")
+	fmt.Printf("こんにちは, κόσμε!\n")
 }
---
+++ code/packages/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("The square root of %d is %g.\n", 21, math.Sqrt(21))
+}
